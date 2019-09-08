d5153901c06ebe982b139c23720f81001ee711e8
Misinkevych <misinkevych.alina@gmail.com>
2019-09-08 15:05:28 +0300

    10-js

--- modul-6/js/task-10.js
+++ modul-6/js/task-10.js
@@ -1,13 +1,52 @@
 'use strict';
 import { users } from './users.js';
 
+// const getSortedUniqueSkills = users => {
+//   return users
+//     .reduce((allSkills, user) => {
+//       allSkills.push(...user.skills);
+//       return allSkills;
+//     }, [])
+//     .filter((user,  arr) => arr.indexOf(user))
+//     .sort();
+// };
+// console.log(getSortedUniqueSkills(users));
+// ---------------------------------------------------
+
 const getSortedUniqueSkills = users => {
   return users
     .reduce((allSkills, user) => {
-      allSkills.push(...user.skills);
+      allSkills.push(...user.skills.filter(arr => !allSkills.includes(arr)));
       return allSkills;
     }, [])
-    .filter((user, index, arr) => arr.indexOf(user) === index)
-    .sort();
+
+    .sort((a, b) => a.localeCompare(b, 'en'));
 };
 console.log(getSortedUniqueSkills(users));
+
+// ======================================================
+// const getSortedUniqueSkills = users => {
+//   return (
+//     users
+//       .reduce((allSkills, user) => {
+//         return [
+//           ...allSkills,
+//           ...user.skills.filter(s => !allSkills.includes(s)),
+//         ];
+//       }, [])
+//       // .filter((user,  arr) => arr.indexOf(user))
+//       .sort()
+//   );
+// };
+// console.log(getSortedUniqueSkills(users));
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// const getSortedUniqueSkills = users => {
+//   return users
+//     .reduce((res, el) => {
+//       return [...res, ...el.skills.filter(s => !res.includes(s))];
+//     }, [])
+//     .sort((a, b) => a.localeCompare(b, 'en'));
+// };
+
+// console.log(getSortedUniqueSkills(users));
